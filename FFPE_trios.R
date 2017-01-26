7d6bf90b13322e3ffa4c026a93916a942097c517
Martina Mijuskovic <martina.mijuskovic@genomicsengland.co.uk>
2017-01-26 18:25:21 +0000

    trios cross-check

--- FFPE_trios.R
+++ FFPE_trios.R
@@ -8,45 +8,63 @@ library("ggvis")
 library("zoo")
 library("ggplot2")
 
-############  Read data (Jan 26 2017) ############  
+rm(list=ls())
 
+############  Read and clean data (Jan 26 2017) ############  
+
+# Read QC data for all cancer samples
+QC_portal <- read.csv("/Users/MartinaMijuskovic/Documents/FFPE/ready_to_upload.01-26-17.csv")
+
+# Summarize and check for dupliactes
+dim(QC_portal)  # 1742 entries total
+sum(duplicated(QC_portal))   # 0 duplicate rows
+sum(duplicated(QC_portal$SAMPLE_WELL_ID))  # 0 duplicates
+sum(duplicated(QC_portal$SAMPLE_LAB_ID))  # 81 duplicates
+
+# Investigate SAMPLE_LAB_ID duplicates
+QC_portal %>% filter(SAMPLE_LAB_ID %in% QC_portal$SAMPLE_LAB_ID[duplicated(QC_portal$SAMPLE_LAB_ID)])   # Duplicate entries for samples that didn't pass QC
+
+# Any duplicate SAMPLE_LAB_IDs in samples that pass pre-sequencing QC? (NONE)
+sum(duplicated(QC_portal %>% filter(SAMPLE_LAB_ID %in% QC_portal$SAMPLE_LAB_ID[duplicated(QC_portal$SAMPLE_LAB_ID)]) %>% filter(PASS_TO_SEQ == "PASS") %>% .$SAMPLE_LAB_ID))  # none
 
 
 ############  Subset trios ############  
 
-# Flag trios (ALL)
-trios <- sapply(unique(QC_data$PATIENT_ID), function(x){
-  if (sum(c("GL", "FFPE", "FF") %in% QC_data[QC_data$PATIENT_ID == x,]$SAMPLE_TYPE) == 3) {
+# Subset samples that pass QC, pass tumor and germline contamination (remove also those with no data available)
+table(QC_portal$PASS_TO_SEQ, exclude = NULL)
+QC_portal_passPreSeqQC <- QC_portal %>% filter(PASS_TO_SEQ == "Pass", TUMOUR_CONTAMINATION != "Fail", GERMLINE_CONTAMINATION != "Fail")   # 489 samples (of 1742)
+table((QC_portal_passPreSeqQC %>% filter(SAMPLE_TYPE %in% c("FF", "FFPE")) %>% .$TUMOUR_CONTAMINATION), exclude = NULL)
+table((QC_portal_passPreSeqQC %>% filter(SAMPLE_TYPE == "GL") %>% .$GERMLINE_CONTAMINATION), exclude = NULL)
+# Remove tumor samples (only) with no contamination data available
+rmIDs <- QC_portal_passPreSeqQC %>% filter(SAMPLE_TYPE %in% c("FF", "FFPE"), TUMOUR_CONTAMINATION == "") %>% .$SAMPLE_LAB_ID
+QC_portal_passPreSeqQC <- QC_portal_passPreSeqQC %>% filter(!SAMPLE_LAB_ID %in% rmIDs)  # 1152 entries
+
+# Flag trios
+trios <- sapply(unique(QC_portal_passPreSeqQC$PATIENT_ID), function(x){
+  if (sum(c("GL", "FFPE", "FF") %in% QC_portal_passPreSeqQC[QC_portal_passPreSeqQC$PATIENT_ID == x,]$SAMPLE_TYPE) == 3) {
     return(1)
   }
   else {
     return(0)
   }
 })
+names(trios) <- unique(QC_portal_passPreSeqQC$PATIENT_ID)
 
-QC_data$Trio <- 0
-QC_data[QC_data$PATIENT_ID %in% names(trios[trios == 1]),]$Trio <- 1
+QC_portal_passPreSeqQC$Trio <- 0
+QC_portal_passPreSeqQC[QC_portal_passPreSeqQC$PATIENT_ID %in% names(trios[trios == 1]),]$Trio <- 1
 
-# Total number of trios in the database
-dim(QC_data %>% filter(Trio == 1))  # 325
+# Total number of trios that pass QC to sequencing
+dim(QC_portal_passPreSeqQC %>% filter(Trio == 1))  # 78
+table((QC_portal_passPreSeqQC %>% filter(Trio == 1, SAMPLE_TYPE %in% c("FF", "FFPE")) %>% .$TUMOUR_CONTAMINATION), exclude=NULL)
 
-# Flag trios where all three samples pass QC before sequencing
-QC_data_PassToSeq <- QC_data %>% filter(PASS_TO_SEQ == "Pass")   # 1091 samples (of 1397) pass QC for sequencing
+# Subset data for clean trios
+QC_portal_trios <- QC_portal_passPreSeqQC %>% filter(Trio == 1)
 
-triosQC <- sapply(unique(QC_data_PassToSeq$PATIENT_ID), function(x){
-  if (sum(c("GL", "FFPE", "FF") %in% QC_data_PassToSeq[QC_data_PassToSeq$PATIENT_ID == x,]$SAMPLE_TYPE) == 3) {
-    return(1)
-  }
-  else {
-    return(0)
-  }
-})
+# Load trios list from Alona (only now became available!)
+Alonas_trios <- read.table("/Users/MartinaMijuskovic/Documents/FFPE/trios.2016-01-26.txt", sep = "\t")
 
-QC_data_PassToSeq$Trio <- 0
-QC_data_PassToSeq[QC_data_PassToSeq$PATIENT_ID %in% names(triosQC[triosQC == 1]),]$Trio <- 1
-
-# Total number of trios that pass pre-sequencing QC
-QC_trios <- QC_data_PassToSeq %>% filter(Trio == 1)  # 153 (51 patients total) --- NO, carefull, there are DUPLICATES
+# Check my trios against Alona's
+sum(!Alonas_trios[Alonas_trios$V2 == "",]$V1 %in% QC_portal_trios$PATIENT_ID)  # 2 missing  ---- check - do not filter PASS_TO_SEQ "TBDs"
 
 ############  Check status of trios in the upload report ############  
 
