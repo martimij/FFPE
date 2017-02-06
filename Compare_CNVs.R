0d6caa60e38f289444e12ad8dd988371b992e074
Martina Mijuskovic <martina.mijuskovic@genomicsengland.co.uk>
2017-02-06 16:50:39 +0000

    CNV plots

--- Compare_CNVs.R
+++ Compare_CNVs.R
@@ -2,13 +2,29 @@
 # FFPE project
 # Reads the FFPE trio manifest, finds corresponding VCF files and compares Canvas CNVs between FF and FFPE using bedtools (on HPC)
 
+
+### Install R packages if not present
+# install.packages("dplyr", lib = "~/R/x86_64-pc-linux-gnu-library/3.3")
+# NOTE that to install XML and Rcurl, libcurl4-openssl-dev and libxml2-dev are needed on the system
+# See: http://stackoverflow.com/questions/10965755/genomicfeatures-package-installation-trouble
+# install.packages(c("XML", "RCurl"), lib = "~/R/x86_64-pc-linux-gnu-library/3.3")
+# source("https://bioconductor.org/biocLite.R") # Install in the local library "~/R/x86_64-pc-linux-gnu-library/3.3"
+# biocLite("VariantAnnotation", lib.loc = "~/R/x86_64-pc-linux-gnu-library/3.3", lib = "~/R/x86_64-pc-linux-gnu-library/3.3")
+# install.packages("VennDiagram", lib = "~/R/x86_64-pc-linux-gnu-library/3.3")
+
 library(dplyr)
 library(VariantAnnotation)
+library(ggplot2)
 
-setwd("/home/mmijuskovic/FFPE")
+setwd("/home/mmijuskovic/FFPE/CNV_trio_comparison")
+
+today <- Sys.Date()
 
 # Load the manifest
-QC_portal_trios <- read.csv("/home/mmijuskovic/FFPE/QC_portal_trios.csv")
+QC_portal_trios <- read.csv("/home/mmijuskovic/FFPE/CNV_trio_comparison/QC_portal_trios.csv")
+
+# Subset for FF and FFPE samples
+QC_portal_trios <- QC_portal_trios %>% filter(SAMPLE_TYPE %in% c("FF", "FFPE"))
 
 # Get VCF paths
 QC_portal_trios$VCF_path <- as.character(sapply(QC_portal_trios$BamPath, function(x){
@@ -16,35 +32,13 @@ QC_portal_trios$VCF_path <- as.character(sapply(QC_portal_trios$BamPath, functio
   system(command, intern = T)
 }))
 
-<<<<<<< HEAD
-# # # Test get VCF paths
-# dummy_bam_path <- "/Users/MartinaMijuskovic/FFPE/Trio_VCFs/"
-# QC_portal_trios2 <- QC_portal_trios %>% filter(SAMPLE_WELL_ID %in% c("LP2000906-DNA_A02", "LP2000907-DNA_B01", "LP3000070-DNA_G01", "LP3000079-DNA_B02"))
-# QC_portal_trios2$VCF_path <- sapply(rep(dummy_bam_path, 4), function(x){
-#   command <- paste("find", x, "-iname *.SV.vcf.gz", sep = " ")
-#   system(command, intern = T)
-# })
-# QC_portal_trios2$VCF_path <- QC_portal_trios2$VCF_path[1:4]
 
 # Function that compares CNVs from FFPE trios (VCFs copied locally)
-=======
-# # Test get VCF paths
-dummy_bam_path <- "/Users/MartinaMijuskovic/FFPE/Trio_VCFs/"
-QC_portal_trios2 <- QC_portal_trios %>% filter(SAMPLE_WELL_ID %in% c("LP2000906-DNA_A02", "LP2000907-DNA_B01", "LP3000070-DNA_G01", "LP3000079-DNA_B02"))
-QC_portal_trios2$VCF_path <- sapply(rep(dummy_bam_path, 4), function(x){
-  command <- paste("find", x, "-iname *.SV.vcf.gz", sep = " ")
-  system(command, intern = T)
-})
-QC_portal_trios2$VCF_path <- QC_portal_trios2$VCF_path[1:4]
-
-# Function that compares CNVs from FFPE trios (VCFs copied locally)
-
->>>>>>> 775483df6c951699042bffdf0dd2a6fac8950712
 compareCNV <- function(patientID){
   
   # Get FF and FFPE VCF paths for specified patient ID and read into a Large CollapsedVCF object (VariantAnnotation package)
-  ff_path <- QC_portal_trios2 %>% filter(PATIENT_ID == patientID, SAMPLE_TYPE == "FF") %>% .$VCF_path
-  ffpe_path <- QC_portal_trios2 %>% filter(PATIENT_ID == patientID, SAMPLE_TYPE == "FFPE") %>% .$VCF_path
+  ff_path <- QC_portal_trios %>% filter(PATIENT_ID == patientID, SAMPLE_TYPE == "FF") %>% .$VCF_path
+  ffpe_path <- QC_portal_trios %>% filter(PATIENT_ID == patientID, SAMPLE_TYPE == "FFPE") %>% .$VCF_path
   ff_vcf <- readVcf(ff_path)
   ffpe_vcf <- readVcf(ffpe_path)
   
@@ -93,14 +87,14 @@ compareCNV <- function(patientID){
   
   ### Call bedtools to compare FF and FFPE
   
-  # Add new type coding ("+" is gain, "-" is loss)
+  # Add new "Type2" ("+" is gain, "-" is loss, to separately compare them via bedtools)
   Canvas_ff$Type2 <- NA
   Canvas_ff$Type2 <- sapply(1:dim(Canvas_ff)[1], function(x){
     if (Canvas_ff$Type[x] == "LOSS") {Canvas_ff$Type2[x] <- "-"}
     else if (Canvas_ff$Type[x] == "GAIN") {Canvas_ff$Type2[x] <- "+"}
   })
-  # Add new type coding ("+" is gain, "-" is loss)
-  Canvas_ffpe$Type2 <- NA
+
+    Canvas_ffpe$Type2 <- NA
   Canvas_ffpe$Type2 <- sapply(1:dim(Canvas_ffpe)[1], function(x){
     if (Canvas_ffpe$Type[x] == "LOSS") {Canvas_ffpe$Type2[x] <- "-"}
     else if (Canvas_ffpe$Type[x] == "GAIN") {Canvas_ffpe$Type2[x] <- "+"}
@@ -124,7 +118,7 @@ compareCNV <- function(patientID){
   # Call bedtools to get FF overlap with FFPE
   system(paste("/home/mmijuskovic/bedtools2/bin/bedtools coverage -s -a", paste0(patientID, "_CNV_ffpe.bed"), "-b", paste0(patientID, "_CNV_ff.bed"), ">", paste0(patientID, "_CNV_ffpe_overlap.bed")), intern = T)
   ffpe_overlap <- read.table(paste0(patientID, "_CNV_ffpe_overlap.bed"), sep = "\t")
-  names(ffpe_overlap) <- c(names(ff_bed), "NumOverlap", "BPoverlap", "BPTotal", "PCT")
+  names(ffpe_overlap) <- c(names(ffpe_bed), "NumOverlap", "BPoverlap", "BPTotal", "PCT")
   
   # Summary table
   result <- data.frame(PERCENT_RECALL_FF = sum(ff_overlap$BPoverlap) / sum(ff_overlap$BPTotal), PERCENT_RECALL_FFPE = sum(ffpe_overlap$BPoverlap) / sum(ffpe_overlap$BPTotal), BP_OVERLAP = sum(ff_overlap$BPoverlap), BP_FF_ONLY = (sum(ff_overlap$BPTotal) - sum(ff_overlap$BPoverlap)), BP_FFPE_ONLY = (sum(ffpe_overlap$BPTotal) - sum(ffpe_overlap$BPoverlap)))
@@ -132,34 +126,97 @@ compareCNV <- function(patientID){
 }
 
 # Get all patient IDs
-<<<<<<< HEAD
 patientIDs <- unique(QC_portal_trios$PATIENT_ID)
-=======
-patientIDs <- unique(QC_portal_trios2$PATIENT_ID)
->>>>>>> 775483df6c951699042bffdf0dd2a6fac8950712
 
 # Run CNV comparison for each patient ID
 CNV_summary <- lapply(patientIDs, compareCNV)
-names(CNV_summary) <- patientIDs
-<<<<<<< HEAD
-# Convert to data.frame --- continue here
+CNV_summary <- bind_rows(CNV_summary)
+CNV_summary$PATIENT_ID <- patientIDs
 
-# Write the output table
+# Write out the resulting table
+write.csv(CNV_summary, file = paste0("CNV_summary_", today, ".csv"), row.names = F, quote = F)
+
+# Read the result table
+#CNV_summary <- read.csv(paste0("CNV_summary_", today, ".csv"))
+
+### Create plots
+
+# Add QC details to the CNV summary table
+QC_table <- QC_portal_trios %>% filter(SAMPLE_TYPE == "FFPE") %>% select(PATIENT_ID, CENTER_CODE, TumorType, SAMPLE_WELL_ID, TUMOUR_PURITY, GC_DROP, AT_DROP, COVERAGE_HOMOGENEITY, CHIMERIC_PER, AV_FRAGMENT_SIZE_BP, MAPPING_RATE_PER)
+names(QC_table)[4:11] <- paste0("FFPE_", names(QC_table)[4:11])
+QC_table <- left_join(QC_table, (QC_portal_trios %>% filter(SAMPLE_TYPE == "FF") %>% select(PATIENT_ID, SAMPLE_WELL_ID, LIBRARY_TYPE, TUMOUR_PURITY, GC_DROP, AT_DROP, COVERAGE_HOMOGENEITY, CHIMERIC_PER, AV_FRAGMENT_SIZE_BP, MAPPING_RATE_PER)), by = "PATIENT_ID")
+names(QC_table)[12:20] <- paste0("FF_", names(QC_table)[12:20])
+CNV_summary <- left_join(CNV_summary, QC_table, by = "PATIENT_ID")
+
+# Recall of FF vs recall of FFPE
+ggplot(CNV_summary, aes(x = PERCENT_RECALL_FF, y = PERCENT_RECALL_FFPE)) + geom_jitter() + geom_smooth(method = "lm") + labs(x = "Percent Recall of FF", y = "Percent Recall of FFPE") 
+
+# Recall of FF by AT dropout/coverage homogeneity of FFPE, colored by center
+ggplot(CNV_summary, aes(x = PERCENT_RECALL_FF, y = FFPE_AT_DROP, col = factor(CENTER_CODE))) + geom_jitter() + labs(x = "Percent Recall of FF", y = "FFPE AT Dropout") + theme(legend.title=element_blank())
+ggplot(CNV_summary, aes(x = PERCENT_RECALL_FF, y = FFPE_COVERAGE_HOMOGENEITY, col = factor(CENTER_CODE))) + geom_jitter() + labs(x = "Percent Recall of FF", y = "FFPE Unevenness of Coverage") + theme(legend.title=element_blank())
+
+# Recall of FF by AT dropout of FFPE, colored by tumor type
+ggplot(CNV_summary, aes(x = PERCENT_RECALL_FF, y = FFPE_AT_DROP, col = factor(TumorType))) + geom_jitter() + labs(x = "Percent Recall of FF", y = "FFPE AT Dropout") + theme(legend.title=element_blank())
+ggplot(CNV_summary, aes(x = PERCENT_RECALL_FF, y = FFPE_COVERAGE_HOMOGENEITY, col = factor(TumorType))) + geom_jitter() + labs(x = "Percent Recall of FF", y = "FFPE Unevenness of Coverage") + theme(legend.title=element_blank())
+
+# Recall by tumor purity
+ggplot(CNV_summary, aes(x = PERCENT_RECALL_FF, y = FFPE_TUMOUR_PURITY, col = factor(TumorType))) + geom_jitter() + labs(x = "Percent Recall of FF", y = "FFPE Tumour Purity")  + theme(legend.title=element_blank())
+ggplot(CNV_summary, aes(x = PERCENT_RECALL_FF, y = FF_TUMOUR_PURITY, col = factor(TumorType))) + geom_jitter() + labs(x = "Percent Recall of FF", y = "FF Tumour Purity")  + theme(legend.title=element_blank())
+# Recall by difference in tumor purity between FF and FFPE
+CNV_summary$PurityDiff <- CNV_summary$FFPE_TUMOUR_PURITY-CNV_summary$FF_TUMOUR_PURITY
+ggplot(CNV_summary, aes(x = PERCENT_RECALL_FF, y = PurityDiff, col = factor(TumorType))) + geom_jitter() + labs(x = "Percent Recall of FF", y = "Difference In Tumour Purity")  + theme(legend.title=element_blank())
+ggplot(CNV_summary, aes(x = PERCENT_RECALL_FF, y = FFPE_AT_DROP, col = factor(PurityDiff > 15))) + geom_jitter() + labs(x = "Percent Recall of FF", y = "FFPE AT Dropout")  + theme(legend.title=element_blank())
+
+# Recall by high vs low FF and FFPE purity (both > 50)
+ggplot(CNV_summary, aes(x = PERCENT_RECALL_FF, y = FFPE_AT_DROP, col = factor((FFPE_TUMOUR_PURITY > 50) & (FF_TUMOUR_PURITY > 50)))) + geom_jitter() + labs(x = "Percent Recall of FF", y = "FFPE AT Dropout") + theme(legend.title=element_blank())
+
+# Divide samples into good and bad FF recall (>50%), see which variables make a difference
+ggplot(data=CNV_summary, aes(x=(PERCENT_RECALL_FF>0.5), y=FFPE_AT_DROP, fill=(PERCENT_RECALL_FF>0.5))) + geom_boxplot() + geom_jitter() + labs(x = "High FF CNV Recall (>50%)", y = "FFPE AT Dropout") + theme(legend.title=element_blank())
+ggplot(data=CNV_summary, aes(x=(PERCENT_RECALL_FF>0.5), y=FFPE_COVERAGE_HOMOGENEITY, fill=(PERCENT_RECALL_FF>0.5))) + geom_boxplot() + geom_jitter() + labs(x = "High FF CNV Recall (>50%)", y = "FFPE Unevenness of Coverage") + theme(legend.title=element_blank())
+
+# Recall by FFPE tumor purity
+ggplot(CNV_summary, aes(x = PERCENT_RECALL_FF, y = FFPE_TUMOUR_PURITY)) + geom_jitter()
+ggplot(data=CNV_summary, aes(x=(PERCENT_RECALL_FF>0.5), y=FFPE_TUMOUR_PURITY, fill=(PERCENT_RECALL_FF>0.5))) + geom_boxplot() + geom_jitter() + labs(x = "High FF CNV Recall (>50%)", y = "FFPE Tumour Purity") + theme(legend.title=element_blank())
+
+# Recall by % chimeric fragments
+ggplot(data=CNV_summary, aes(x=(PERCENT_RECALL_FF>0.5), y=FFPE_CHIMERIC_PER, fill=(PERCENT_RECALL_FF>0.5))) + geom_boxplot() + geom_jitter() + labs(x = "High FF CNV Recall (>50%)", y = "FFPE Chimeric Frag") + theme(legend.title=element_blank())
 
 
-=======
->>>>>>> 775483df6c951699042bffdf0dd2a6fac8950712
+# Recall by tumor type
+ggplot(data=CNV_summary, aes(x=TumorType, y=PERCENT_RECALL_FF)) + geom_boxplot() + geom_jitter(aes(col = TumorType)) + labs(x = "Tumor Type", y = "Percent CNV Recall") + theme(legend.title=element_blank())
 
-# ### Test function locally
+# Recall by GMC, color by tumor type
+ggplot(data=CNV_summary, aes(x=CENTER_CODE, y=PERCENT_RECALL_FF)) + geom_boxplot() + geom_jitter(aes(col = TumorType)) + labs(x = "GMC", y = "Percent CNV Recall") + theme(legend.title=element_blank())
+
+# Recall by FF library type
+ggplot(data=CNV_summary, aes(x=FF_LIBRARY_TYPE, y=PERCENT_RECALL_FF)) + geom_boxplot() + geom_jitter(aes(col = TumorType)) + labs(x = "FF Library Type", y = "Percent CNV Recall") + theme(legend.title=element_blank())
+
+
+
+
+
+### Test function locally
+
+# patientIDs <- c("212000009", "217000028")
+# 
+# # # Test get VCF paths
+# dummy_bam_path <- "/Users/MartinaMijuskovic/FFPE/Trio_VCFs/"
+# QC_portal_trios2 <- QC_portal_trios %>% filter(SAMPLE_WELL_ID %in% c("LP2000906-DNA_A02", "LP2000907-DNA_B01", "LP3000070-DNA_G01", "LP3000079-DNA_B02"))
+# QC_portal_trios2$VCF_path <- sapply(rep(dummy_bam_path, 4), function(x){
+#   command <- paste("find", x, "-iname *.SV.vcf.gz", sep = " ")
+#   system(command, intern = T)
+# })
+# QC_portal_trios2$VCF_path <- QC_portal_trios2$VCF_path[1:4]
+# 
 # 
 # compareCNV <- function(patientID){
-#   
+# 
 #   # Get FF and FFPE VCF paths for specified patient ID and read into a Large CollapsedVCF object (VariantAnnotation package)
 #   ff_path <- QC_portal_trios2 %>% filter(PATIENT_ID == patientID, SAMPLE_TYPE == "FF") %>% .$VCF_path
 #   ffpe_path <- QC_portal_trios2 %>% filter(PATIENT_ID == patientID, SAMPLE_TYPE == "FFPE") %>% .$VCF_path
 #   ff_vcf <- readVcf(ff_path)
 #   ffpe_vcf <- readVcf(ffpe_path)
-#   
+# 
 #   ### FF info
 #   # Extract info fields
 #   SVinfo_ff <- as.data.frame(info(ff_vcf))
@@ -180,7 +237,7 @@ names(CNV_summary) <- patientIDs
 #   Canvas_ff$Type <-  sapply(1:dim(Canvas_ff)[1], function(x){strsplit(Canvas_ff$ID[x], split = ":")[[1]][2]})
 #   # Keep LOSS/GAIN only
 #   Canvas_ff <- Canvas_ff %>% filter(Type %in% c("LOSS", "GAIN"))
-#   
+# 
 #   ### FFPE info
 #   # Extract info fields
 #   SVinfo_ffpe <- as.data.frame(info(ffpe_vcf))
@@ -201,10 +258,10 @@ names(CNV_summary) <- patientIDs
 #   Canvas_ffpe$Type <-  sapply(1:dim(Canvas_ffpe)[1], function(x){strsplit(Canvas_ffpe$ID[x], split = ":")[[1]][2]})
 #   # Keep LOSS/GAIN only
 #   Canvas_ffpe <- Canvas_ffpe %>% filter(Type %in% c("LOSS", "GAIN"))  # 7
-#   
-#   
+# 
+# 
 #   ### Call bedtools to compare FF and FFPE
-#   
+# 
 #   # Add new type coding ("+" is gain, "-" is loss)
 #   Canvas_ff$Type2 <- NA
 #   Canvas_ff$Type2 <- sapply(1:dim(Canvas_ff)[1], function(x){
@@ -227,19 +284,26 @@ names(CNV_summary) <- patientIDs
 #   # Write bed files indicating gain as "+" strand and loss as "-" strand (to enable comparing them at once in bedtools)
 #   write.table(ff_bed, file = paste0(patientID, "_CNV_ff.bed"), quote = F, row.names = F, col.names = F, sep = "\t")
 #   write.table(ffpe_bed, file = paste0(patientID, "_CNV_ffpe.bed"), quote = F, row.names = F, col.names = F, sep = "\t")
-#   
+# 
 #   # Call bedtools to get FFPE overlap with FF
 #   system(paste("bedtools coverage -s -a", paste0(patientID, "_CNV_ff.bed"), "-b", paste0(patientID, "_CNV_ffpe.bed"), ">", paste0(patientID, "_CNV_ff_overlap.bed")), intern = T)
 #   ff_overlap <- read.table(paste0(patientID, "_CNV_ff_overlap.bed"), sep = "\t")
 #   names(ff_overlap) <- c(names(ff_bed), "NumOverlap", "BPoverlap", "BPTotal", "PCT")
-#   
+# 
 #   # Call bedtools to get FF overlap with FFPE
 #   system(paste("bedtools coverage -s -a", paste0(patientID, "_CNV_ffpe.bed"), "-b", paste0(patientID, "_CNV_ff.bed"), ">", paste0(patientID, "_CNV_ffpe_overlap.bed")), intern = T)
 #   ffpe_overlap <- read.table(paste0(patientID, "_CNV_ffpe_overlap.bed"), sep = "\t")
 #   names(ffpe_overlap) <- c(names(ff_bed), "NumOverlap", "BPoverlap", "BPTotal", "PCT")
-#   
+# 
 #   # Summary table
-#   result <- data.frame(PATIENT_ID = patientID, PERCENT_RECALL_FF = sum(ff_overlap$BPoverlap) / sum(ff_overlap$BPTotal), PERCENT_RECALL_FFPE = sum(ffpe_overlap$BPoverlap) / sum(ffpe_overlap$BPTotal), BP_OVERLAP = sum(ff_overlap$BPoverlap), BP_FF_ONLY = (sum(ff_overlap$BPTotal) - sum(ff_overlap$BPoverlap)), BP_FFPE_ONLY = (sum(ffpe_overlap$BPTotal) - sum(ffpe_overlap$BPoverlap)))
+#   result <- data.frame(PERCENT_RECALL_FF = sum(ff_overlap$BPoverlap) / sum(ff_overlap$BPTotal), PERCENT_RECALL_FFPE = sum(ffpe_overlap$BPoverlap) / sum(ffpe_overlap$BPTotal), BP_OVERLAP = sum(ff_overlap$BPoverlap), BP_FF_ONLY = (sum(ff_overlap$BPTotal) - sum(ff_overlap$BPoverlap)), BP_FFPE_ONLY = (sum(ffpe_overlap$BPTotal) - sum(ffpe_overlap$BPoverlap)))
 #   return(result)
 # }
 # 
+# # Run CNV comparison for each patient ID
+# CNV_summary <- lapply(patientIDs, compareCNV)
+# CNV_summary <- bind_rows(CNV_summary)
+# CNV_summary$PATIENT_ID <- patientIDs
+# 
+# # Write out the resulting table
+# write.csv(CNV_summary, col.names = T, row.names = F, quote = F)
